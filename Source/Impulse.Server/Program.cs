97fab908aa57e9b2342acbd67325ba264077fca6
Jorge Candeias <jorge.candeias@outcompute.com>
2023-07-29 21:15:49 +0100

    Added silo configuration

--- Source/Impulse.Server/Program.cs
+++ Source/Impulse.Server/Program.cs
@@ -1,17 +1,135 @@
 ﻿using Impulse.Grains;
+using Impulse.Server;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Orleans.Configuration;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
 
-var host = Host
-    .CreateDefaultBuilder()
-    .UseOrleans(orleans =>
+var ports = new
+{
+    Primary = 11111,
+    Silo = TcpUtility.GetAvailablePort(11111, 11999),
+    Gateway = TcpUtility.GetAvailablePort(30000, 30999),
+    Dashboard = TcpUtility.GetAvailablePort(40000, 40999)
+};
+
+var builder = Host.CreateApplicationBuilder();
+
+// hardcode the target enrivonment for sample purposes
+builder.Environment.EnvironmentName = Environments.Development;
+
+// add appropriate loggers per environment
+builder.Logging.ClearProviders();
+if (builder.Environment.IsDevelopment())
+{
+    // the console logger must only run in development due its performance impact
+    builder.Logging.AddConsole();
+}
+else
+{
+    // add production loggers
+}
+
+// add orleans services for any environment
+builder.UseOrleans(orleans =>
+{
+    orleans
+        .AddActivityGrainCallFilter(options =>
+        {
+            options.AllowedAssemblies.Add(typeof(ActivityGrainCallFilterOptions).Assembly);
+        })
+        .AddActivityPropagation()
+        .AddMemoryStreams("Chat")
+        .UseDashboard(options =>
+        {
+            options.Port = ports.Dashboard;
+        });
+});
+
+// add orleans services as appropriate for each environment
+if (builder.Environment.IsDevelopment())
+{
+    builder.UseOrleans(orleans =>
     {
         orleans
-            .UseLocalhostClustering()
-            .AddActivityGrainCallFilter(options =>
+            .UseLocalhostClustering(ports.Silo, ports.Gateway, new IPEndPoint(IPAddress.Loopback, ports.Primary))
+            .AddMemoryGrainStorageAsDefault()
+            .AddMemoryGrainStorage("PubSubStore");
+    });
+}
+else
+{
+    builder.UseOrleans(orleans =>
+    {
+        orleans
+            .ConfigureEndpoints(ports.Silo, ports.Gateway, AddressFamily.InterNetwork, true)
+            .Configure<ClusterOptions>(options =>
             {
-                options.AllowedAssemblies.Add(typeof(ActivityGrainCallFilterOptions).Assembly);
+                options.ClusterId = nameof(Impulse);
+                options.ServiceId = nameof(Impulse);
+            })
+            .UseAdoNetClustering(options =>
+            {
+                options.Invariant = "Microsoft.Data.SqlClient";
+                options.ConnectionString = builder.Configuration.GetConnectionString("Orleans");
+            })
+            .AddAdoNetGrainStorageAsDefault(options =>
+            {
+                options.Invariant = "Microsoft.Data.SqlClient";
+                options.ConnectionString = builder.Configuration.GetConnectionString("Orleans");
+            })
+            .AddAdoNetGrainStorage("PubSubStore", options =>
+            {
+                options.Invariant = "Microsoft.Data.SqlClient";
+                options.ConnectionString = builder.Configuration.GetConnectionString("Orleans");
+            })
+            .UseAdoNetReminderService(options =>
+            {
+                options.Invariant = "Microsoft.Data.SqlClient";
+                options.ConnectionString = builder.Configuration.GetConnectionString("Orleans");
             });
-    })
-    .Build();
+    });
+}
 
-await host.RunAsync();
+// add open telemetry services for all environments
+builder.Services.AddSingleton(sp => new ActivitySource(nameof(Impulse)));
+
+// add open telemetry as appropriate for each environment
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services
+        .AddOpenTelemetry()
+        .WithTracing(options =>
+        {
+            options
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(nameof(Impulse)))
+                .AddSource(nameof(Impulse))
+                .AddConsoleExporter(x =>
+                {
+                    x.Targets = ConsoleExporterOutputTargets.Console | ConsoleExporterOutputTargets.Debug;
+                });
+        });
+}
+else
+{
+    builder.Services
+        .AddOpenTelemetry();
+}
+
+using var host = builder.Build();
+
+await host.StartAsync();
+
+if (Environment.UserInteractive)
+{
+    Console.Title = $"Silo: {ports.Silo}, Gateway: {ports.Gateway}, Dashboard: {ports.Dashboard}";
+}
+
+await host.WaitForShutdownAsync();
---
+++ Source/Impulse.Server/TcpUtility.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+
+namespace Impulse.Server;
+
+internal class TcpUtility
+{
+    public static int GetAvailablePort(int start, int end)
+    {
+        if (!TryGetAvailablePort(start, end, out var port))
+        {
+            throw new InvalidOperationException($"Could not find an available port between '{start}' and '{end}'");
+        }
+
+        return port;
+    }
+
+    public static bool TryGetAvailablePort(int start, int end, out int port)
+    {
+        var length = end - start;
+        for (var i = 0; i <= length; ++i)
+        {
+            port = start + i;
+            if (TryPort(port))
+            {
+                return true;
+            }
+        }
+
+        port = -1;
+        return false;
+    }
+
+    private static bool TryPort(int port)
+    {
+        var listener = TcpListener.Create(port);
+        try
+        {
+            listener.ExclusiveAddressUse = true;
+            listener.Start();
+            return true;
+        }
+        catch (SocketException)
+        {
+            return false;
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+}
