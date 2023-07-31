71b029c68c1ab9c85973153317a683fe82406a7a
Jorge Candeias <jorge.candeias@outcompute.com>
2023-07-31 22:35:01 +0100

    Added tests

---
+++ Tests/Impulse.Grains.Tests/ActiveChatRoomGrainTests.cs
@@ -0,0 +1,117 @@
+﻿using Impulse.Core;
+using Orleans.Streams;
+
+namespace Impulse.Grains.Tests;
+
+[Collection(nameof(TestClusterCollection))]
+public class ActiveChatRoomGrainTests
+{
+    public ActiveChatRoomGrainTests(TestClusterFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    private readonly TestClusterFixture _fixture;
+
+    [Fact]
+    public async Task JoinsUser()
+    {
+        // arrange
+        var userName = Guid.NewGuid().ToString();
+        var roomName = Guid.NewGuid().ToString();
+        var user = await _fixture.TestCluster.Client.GetChatUsersIndexGrain().GetOrAdd(userName);
+
+        // act
+        var completion = new TaskCompletionSource<ChatMessage>();
+        await _fixture.TestCluster.Client
+            .GetStreamProvider("Chat")
+            .GetStream<ChatMessage>(roomName)
+            .SubscribeAsync((message, token) =>
+            {
+                completion.SetResult(message);
+
+                return Task.CompletedTask;
+            });
+
+        await _fixture.TestCluster.Client.GetActiveChatRoomGrain(roomName).Join(user);
+
+        var result = await completion.Task.WaitAsync(TimeSpan.FromSeconds(10));
+
+        // assert
+        Assert.NotNull(result);
+        Assert.Equal("System", result.User);
+        Assert.Equal(roomName, result.Room);
+        Assert.Equal($"{userName} joined chat room {roomName}", result.Text);
+    }
+
+    [Fact]
+    public async Task LeavesUser()
+    {
+        // arrange
+        var userName = Guid.NewGuid().ToString();
+        var roomName = Guid.NewGuid().ToString();
+        var user = await _fixture.TestCluster.Client.GetChatUsersIndexGrain().GetOrAdd(userName);
+        await _fixture.TestCluster.Client.GetActiveChatRoomGrain(roomName).Join(user);
+
+        // act
+        var completion = new TaskCompletionSource<ChatMessage>();
+        await _fixture.TestCluster.Client
+            .GetStreamProvider("Chat")
+            .GetStream<ChatMessage>(roomName)
+            .SubscribeAsync((message, token) =>
+            {
+                // skip the joined message in case of race condition
+                if (message.Text.Contains("joined"))
+                {
+                    return Task.CompletedTask;
+                }
+
+                completion.SetResult(message);
+
+                return Task.CompletedTask;
+            });
+
+        await _fixture.TestCluster.Client.GetActiveChatRoomGrain(roomName).Leave(user);
+
+        var result = await completion.Task.WaitAsync(TimeSpan.FromSeconds(10));
+
+        // assert
+        Assert.NotNull(result);
+        Assert.Equal("System", result.User);
+        Assert.Equal(roomName, result.Room);
+        Assert.Equal($"{userName} left chat room {roomName}", result.Text);
+    }
+
+    [Fact]
+    public async Task Messages()
+    {
+        // arrange
+        var userName = Guid.NewGuid().ToString();
+        var roomName = Guid.NewGuid().ToString();
+        var user = await _fixture.TestCluster.Client.GetChatUsersIndexGrain().GetOrAdd(userName);
+
+        // act
+        var completion = new TaskCompletionSource<ChatMessage>();
+        await _fixture.TestCluster.Client
+            .GetStreamProvider("Chat")
+            .GetStream<ChatMessage>(roomName)
+            .SubscribeAsync((message, token) =>
+            {
+                completion.SetResult(message);
+
+                return Task.CompletedTask;
+            });
+
+        var message = new ChatMessage(Guid.NewGuid(), roomName, userName, "Some Text");
+        await _fixture.TestCluster.Client.GetActiveChatRoomGrain(roomName).Message(message);
+
+        var result = await completion.Task.WaitAsync(TimeSpan.FromSeconds(10));
+
+        // assert
+        Assert.NotNull(result);
+        Assert.Equal(message.Guid, result.Guid);
+        Assert.Equal(message.User, result.User);
+        Assert.Equal(message.Room, result.Room);
+        Assert.Equal(message.Text, result.Text);
+    }
+}
--- Tests/Impulse.Grains.Tests/TestClusterFixture.cs
+++ Tests/Impulse.Grains.Tests/TestClusterFixture.cs
@@ -3,23 +3,19 @@ using Microsoft.Extensions.Configuration;
 
 namespace Impulse.Grains.Tests;
 
-public sealed class TestClusterFixture : IDisposable
+public sealed class TestClusterFixture
 {
     public TestClusterFixture()
     {
         TestCluster = new TestClusterBuilder()
             .AddSiloBuilderConfigurator<TestSiloConfigurator>()
+            .AddClientBuilderConfigurator<TestClientConfigurator>()
             .Build();
 
         TestCluster.Deploy();
     }
 
     public TestCluster TestCluster { get; }
-
-    public void Dispose()
-    {
-        TestCluster.StopAllSilos();
-    }
 
     private class TestSiloConfigurator : ISiloConfigurator
     {
